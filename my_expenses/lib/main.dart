15ac8de36b2678432294e876ce649892fc11e978
Amit Bhave <amitbhave10@gmail.com>
2020-04-26 19:34:15 +0530

    Personal expense project initial commit

---
+++ my_expenses/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+        Card(
+          color: Colors.blue,
+          child: Container(
+              width: double.infinity,
+              child: Text("Chart!")),
+          elevation: 5,
+        ),
+        Card(
+          child: Text("Transaction List!"),
+        )
+      ],),
+    );
+  }
+}
