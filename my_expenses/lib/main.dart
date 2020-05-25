758d9821076cf37f0d55f088d7181c7f826715e5
Amit Bhave <amitbhave10@gmail.com>
2020-05-25 20:26:44 +0530

    Calculate height dynamically

--- my_expenses/lib/main.dart
+++ my_expenses/lib/main.dart
@@ -79,22 +79,29 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appbar = AppBar(
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddNewExpense(context),
+        )
+      ],
+      title: Text('My Expenses'),
+    );
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _startAddNewExpense(context),
-          )
-        ],
-        title: Text('My Expenses'),
-      ),
+      appBar: appbar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-          Chart(_recentTransactions),
-           TransactionList(_userTransactions, _deleteTransaction),
+          Container(
+            height: (MediaQuery.of(context).size.height - appbar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.4,
+            child: Chart(_recentTransactions)
+          ),
+           Container(
+               height: (MediaQuery.of(context).size.height - appbar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.6,
+               child: TransactionList(_userTransactions, _deleteTransaction)
+           ),
         ],),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
